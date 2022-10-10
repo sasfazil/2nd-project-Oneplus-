7393ea5a591fc6651ac3940db853709fd3090077
SHAIK FAZIL <shaikfazil.1112@gmail.com>
2022-10-10 20:27:22 +0530

    commit

---
+++ index (1).js
@@ -0,0 +1,11 @@
+function loadcoupon() {
+    document.getElementById('coupon').style.visibility = "visible";
+    document.getElementById('main').style.opacity = '0.7'
+    document.getElementById('navbar').style.opacity = '0.7'
+
+}
+function closecoupon() {
+    document.getElementById('coupon').style.visibility = "hidden";
+    document.getElementById('main').style.opacity = '1'
+    document.getElementById('navbar').style.opacity = '1'
+}
