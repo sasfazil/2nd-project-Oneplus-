9f58cb12940d08aa8a3faa06803912ef63e7b1b6
SHAIK FAZIL <shaikfazil.1112@gmail.com>
2022-10-26 18:30:12 +0530

    commit

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function darkmode(){
     let darker=document.body
     darker.classList.toggle('darkmode')
     }
+let x = document.getElementById('out');
+let y = document.getElementById('weatherout');
+function geolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+        x.innerText = `Geo location is not working`
+    }
+}
+function showPosition(data) {
+    console.log(data)
+    let lat = data.coords.latitude;
+    let long = data.coords.longitude;
+    x.innerText = `latitude is '${lat}' and longitude is '${long}'`
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    fetch(url, { method: 'GET' })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            let cityName = data.city.name;
+            let temp = data.list[0].temp.day;
+            y.innerText = `${cityName} has ${temp}°C`
+        }
+        )
+
+}
