c394c7f5dfaf7be6ae25a1347c372347cd4601c8
SHAIK FAZIL <shaikfazil.1112@gmail.com>
2022-11-02 22:03:15 +0530

    commit

--- script.js
+++ script.js
@@ -17,25 +17,23 @@ let x = document.getElementById('out');
 let y = document.getElementById('weatherout');
 function geolocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition)
+        navigator.geolocation.getCurrentPosition((data)=> {
+            let lat = data.coords.latitude;
+            let long = data.coords.longitude;
+            x.innerText = `latitude is '${lat}' and longitude is '${long}'`
+            const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+            fetch(url, { method: 'GET' })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    let cityName = data.city.name;
+                    let temp = data.list[0].temp.day;
+                    y.innerText = `${cityName} has ${temp}°C`
+                }
+                )
+        
+        })
     } else {
         x.innerText = `Geo location is not working`
     }
-}
-function showPosition(data) {
-    console.log(data)
-    let lat = data.coords.latitude;
-    let long = data.coords.longitude;
-    x.innerText = `latitude is '${lat}' and longitude is '${long}'`
-    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
-    fetch(url, { method: 'GET' })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            let cityName = data.city.name;
-            let temp = data.list[0].temp.day;
-            y.innerText = `${cityName} has ${temp}°C`
-        }
-        )
-
 }
